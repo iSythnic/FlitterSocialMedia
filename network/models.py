824f14f0f469ac3a1104ec36196b703d1c3605fc
Yuhang <yuhangli.adelaide@gmail.com>
2022-09-05 08:12:24 +1000

    Added Post and Comment Models

--- network/models.py
+++ network/models.py
@@ -1,6 +1,21 @@
+from distutils.command.upload import upload
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Comment_user")
+    content = models.CharField(max_length=200, blank=False)
+
+def filepath(request, filename):
+    return f"posts/image/{request.user.id}/{request.id}/{filename}"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Post_user")
+    headline = models.CharField(max_length=200, blank=True)
+    likes = models.ManyToManyField(User, blank=True, related_name="Post_likes")
+    comments = models.ManyToManyField(Comment, blank=True, related_name="Post_comments")
+    image = models.ImageField(upload_to=filepath, null=True, blank=False)
